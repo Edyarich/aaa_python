215ce533c802d87085b99e73f4165c2b6242868a
Edyarich <ymnik432@gmail.com>
2022-10-27 12:54:58 +0300

    add tests for count-vectorizer

--- count-vectorizer/main.py
+++ count-vectorizer/main.py
@@ -59,3 +59,22 @@ if __name__ == '__main__':
     count_matrix = vectorizer.fit_transform(corpus)
     print(vectorizer.get_feature_names())
     print(count_matrix)
+
+    assert vectorizer.get_feature_names() == [
+        'crock',
+        'pot',
+        'pasta',
+        'never',
+        'boil',
+        'again',
+        'pomodoro',
+        'fresh',
+        'ingredients',
+        'parmesan',
+        'to',
+        'taste'
+    ]
+    assert count_matrix == [
+        [1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    ]
