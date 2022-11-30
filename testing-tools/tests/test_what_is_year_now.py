0321f44a73ef9bbcbc096f8f951396c577ace406
Edyarich <ymnik432@gmail.com>
2022-11-30 17:35:45 +0300

    mock urllib.request.urlopen instead of json.load

--- testing-tools/tests/test_what_is_year_now.py
+++ testing-tools/tests/test_what_is_year_now.py
@@ -2,7 +2,6 @@
 import os
 import sys
 import unittest
-import time
 from unittest.mock import patch, MagicMock
 
 
@@ -13,29 +12,40 @@ sys.path.insert(0, parentdir)
 import what_is_year_now as wiyn
 
 
-REQUEST_DELAY = 3
-
 class TestWIYN(unittest.TestCase):
     """Test function what_is_year_now"""
-    @patch('what_is_year_now.json.load')
-    def test_dmy_format(self, json_dict: MagicMock) -> None:
+    @patch('urllib.request.urlopen')
+    def test_dmy_format(self, mock_urlopen: MagicMock) -> None:
         """Test the correct work with DD.MM.YYYY date format"""
-        json_dict.return_value = {"datetime": "17.11.1999"}
-        time.sleep(REQUEST_DELAY)
-        response = wiyn.what_is_year_now()
-        self.assertEqual(response, 1999)
+        mock = MagicMock()
+        mock.read.return_value = """{
+            "datetime": "17.11.1999"
+        }"""
+        mock.__enter__.return_value = mock
+        mock_urlopen.return_value = mock
 
-    @patch('what_is_year_now.json.load')
-    def test_ymd_format(self, json_dict: MagicMock) -> None:
+        self.assertEqual(wiyn.what_is_year_now(), 1999)
+
+    @patch('urllib.request.urlopen')
+    def test_ymd_format(self, mock_urlopen: MagicMock) -> None:
         """Test the correct work with YYYY-MM-DD date format"""
-        json_dict.return_value = {"datetime": "1999-11-17"}
-        time.sleep(REQUEST_DELAY)
-        response = wiyn.what_is_year_now()
-        self.assertEqual(response, 1999)
+        mock = MagicMock()
+        mock.read.return_value = """{
+            "datetime": "1999-11-17"
+        }"""
+        mock.__enter__.return_value = mock
+        mock_urlopen.return_value = mock
 
-    @patch('what_is_year_now.json.load')
-    def test_exception_raising(self, json_dict: MagicMock) -> None:
+        self.assertEqual(wiyn.what_is_year_now(), 1999)
+
+    @patch('urllib.request.urlopen')
+    def test_exception_raising(self, mock_urlopen: MagicMock) -> None:
         """Check exception raising for any other date format"""
-        json_dict.return_value = {"datetime": "17.11..1999"}
-        time.sleep(REQUEST_DELAY)
+        mock = MagicMock()
+        mock.read.return_value = """{
+            "datetime": "17.11..1999"
+        }"""
+        mock.__enter__.return_value = mock
+        mock_urlopen.return_value = mock
+
         self.assertRaises(ValueError, wiyn.what_is_year_now)
